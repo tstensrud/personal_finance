9b1c9508427fe7c444be34e1c1b08ca852a5ee0f
unknown <torbjorn.stensrud@structor.no>
2024-04-24 14:09:43 +0200

    added css, js and login/register functions

--- pf/views.py
+++ pf/views.py
@@ -1,7 +1,18 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 views = Blueprint("views", __name__)
 
 @views.route('/')
+def index():
+    return render_template("index.html", user=current_user)
+
+@views.route('/home')
+@login_required
 def home():
-    return render_template("home.html")
+    return render_template("home.html", user=current_user)
+
+@views.route('/account')
+@login_required
+def account():
+    return render_template("account.html", user=current_user)
