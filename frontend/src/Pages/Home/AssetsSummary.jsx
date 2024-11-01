4ecabf4a8231080bb93fc60071ff2b0c2f6a31e6
unknown <torbjorn.stensrud@structor.no>
2024-11-01 12:23:33 +0100

    ui ux changes. summary page

--- frontend/src/Pages/Home/AssetsSummary.jsx
+++ frontend/src/Pages/Home/AssetsSummary.jsx
@@ -1,12 +1,13 @@
 import CardBgColor from "../../ui/CardBgColor";
+import AssetsIcon from '../../assets/menusvgs/AssetsIcon.jsx';
 
 function AssetsSummary() {
     return (
         <div className="w-96">
             <CardBgColor>
                 <div className="flex flex-col p-2 w-full">
-                    <div className="w-full text-lg -tracking-wider font-semibold">
-                        Your assets
+                    <div className="flex items-center gap-2 w-full text-lg -tracking-wider font-semibold">
+                        <AssetsIcon /> <span>Your assets</span>
                     </div>
                 </div>
             </CardBgColor>
--- frontend/src/Pages/Home/SecuritySummary.jsx
+++ frontend/src/Pages/Home/SecuritySummary.jsx
@@ -1,12 +1,13 @@
 import CardBgColor from "../../ui/CardBgColor";
+import SecuritiesIcon from '../../assets/menusvgs/StocksIcon.jsx';
 
 function SecuritySummary() {
     return (
         <div className="w-96">
             <CardBgColor>
                 <div className="flex flex-col p-2 w-full">
-                    <div className="w-full text-lg -tracking-wider font-semibold">
-                        Your securities
+                    <div className="flex items-center gap-2 w-full text-lg -tracking-wider font-semibold">
+                        <SecuritiesIcon /> <span>Your securities</span>
                     </div>
                 </div>
             </CardBgColor>
